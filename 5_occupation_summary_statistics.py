45a06f66ff3603eb920dccc1663aef72fb22ab65
alexgoldsmith <46906584+alexgoldsmith@users.noreply.github.com>
2019-06-10 14:21:34 -0400

    asdf
    asdf

--- 5_occupation_summary_statistics.py
+++ 5_occupation_summary_statistics.py
@@ -12,6 +12,7 @@ import os
 os.chdir('D:/Users/Alex/Git_Repositories/Thesis')
 df = pd.read_pickle('SIPP_Dataset_3')
 
-
 sample_size_by_occ_group = df.groupby('industry_pre_birth')['unique_id'].nunique()
+
+print(df['unique_id']).nunique()
 print(sample_size_by_occ_group.sum())
