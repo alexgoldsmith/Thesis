86f01af16ac74919bbb0fff11ef33d760bda31aa
alexgoldsmith <46906584+alexgoldsmith@users.noreply.github.com>
2019-06-13 13:30:12 -0400

    asdf
    asdf

---
+++ tables_sum_stats.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 12:53:17 2019
+
+@author: Alex
+"""
+
+# Import packages
+import pandas as pd
+import os
+
+os.chdir('D:/Users/Alex/Git_Repositories/Thesis')
+df = pd.read_pickle('SIPP_Dataset_3')
+
+sample_size_by_occ_group = df.groupby('industry_pre_birth')['unique_id'].nunique()
+
+print(df['unique_id']).nunique()
+print(sample_size_by_occ_group.sum())
